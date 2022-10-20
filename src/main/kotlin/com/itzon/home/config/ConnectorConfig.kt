272a2cf78f43b418c2d292dd5fcd45a637fb120f
ITZON <itzon.remote@gmail.com>
2022-10-20 14:54:11 +0900

    설정 변경

--- src/main/kotlin/com/itzon/home/config/ConnectorConfig.kt
+++ src/main/kotlin/com/itzon/home/config/ConnectorConfig.kt
@@ -9,30 +9,8 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@Configuration
+//@Configuration
 class ConnectorConfig {
-    @Bean
-    fun servletContainer(): ServletWebServerFactory {
-        val tomcat: TomcatServletWebServerFactory = object : TomcatServletWebServerFactory() {
-            override fun postProcessContext(context: Context) {
-                val securityConstraint = SecurityConstraint()
-                securityConstraint.userConstraint = "CONFIDENTIAL"
-                val collection = SecurityCollection()
-                collection.addPattern("/*")
-                securityConstraint.addCollection(collection)
-                context.addConstraint(securityConstraint)
-            }
-        }
-        tomcat.addAdditionalTomcatConnectors(createSslConnector())
-        return tomcat
-    }
 
-    private fun createSslConnector(): Connector {
-        val connector = Connector("org.apache.coyote.http11.Http11NioProtocol")
-        connector.scheme = "http"
-        connector.secure = false
-        connector.port = 8108
-        connector.redirectPort = 8109
-        return connector
-    }
 }
+
