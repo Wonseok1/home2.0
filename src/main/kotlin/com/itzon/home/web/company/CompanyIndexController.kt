bc30674817478e7912a504dfb37df1be2010d5e4
itzon-jonghyeok <jonghyeok7153@gmail.com>
2022-10-21 15:07:24 +0900

    2022-10-21 CompanyIndexController.kt 추가

--- src/main/kotlin/com/itzon/home/web/company/CompanyIndexController.kt
+++ src/main/kotlin/com/itzon/home/web/company/CompanyIndexController.kt
@@ -8,7 +8,42 @@ class CompanyIndexController {
 
     @GetMapping("/company/company/company")
     fun getCompany() : String {
-        println("getCompany")
         return "views/company/company/company"
+    }
+    @GetMapping("/company/history/history")
+    fun getHistory() : String {
+        return "views/company/history/history"
+    }
+    @GetMapping("/company/project/projectView")
+    fun getProjectView() : String {
+        return "views/company/project/projectView"
+    }
+    @GetMapping("/company/direction/direction")
+    fun getDirection() : String {
+        return "views/company/direction/direction"
+    }
+    @GetMapping("/company/business/business")
+    fun getBusiness() : String {
+        return "views/company/business/business"
+    }
+    @GetMapping("/company/solution/solution")
+    fun getSolution() : String {
+        return "views/company/solution/solution"
+    }
+    @GetMapping("/company/consulting/consulting")
+    fun getConsulting() : String {
+        return "views/company/consulting/consulting"
+    }
+    @GetMapping("/company/job/job")
+    fun getJob() : String {
+        return "views/company/job/job"
+    }
+    @GetMapping("/company/notice/notice")
+    fun getNotice() : String {
+        return "views/company/notice/notice"
+    }
+    @GetMapping("/company/notice/qna")
+    fun getQna() : String {
+        return "views/company/notice/qna"
     }
 }
