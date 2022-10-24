f91d24408b38ecddb77e23b42af4639c000abc0c
ITZON <itzon.remote@gmail.com>
2022-10-24 09:25:10 +0900

    공통 import 구조 약간 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.jcraft.jsch:0.1.55")
+//    implementation("com.jcraft.jsch:0.1.55")
     implementation("com.zaxxer:HikariCP:2.7.8")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
