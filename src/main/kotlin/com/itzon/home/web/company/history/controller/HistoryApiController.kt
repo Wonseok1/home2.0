ed18132c7a5a4b8b1c7f06bfa7d8bbd995ad8dfa
itzon-jonghyeok <jonghyeok7153@gmail.com>
2022-11-01 16:04:20 +0900

    2022-11-01 회사연혁관리 틀

---
+++ src/main/kotlin/com/itzon/home/web/company/history/controller/HistoryApiController.kt
@@ -0,0 +1,12 @@
+package com.itzon.home.web.company.history.controller
+
+import com.itzon.home.common.constant.REST_COMPANY_NOTICE
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("$REST_COMPANY_NOTICE") // /api/company/notice/notice
+class HistoryApiController(
+) {
+
+}
